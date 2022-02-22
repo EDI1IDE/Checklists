d791514b3b2ed28d9dad3d00fcf47559c312b8c9
Edi <muhamedzahiri@gmail.com>
2022-02-22 18:45:01 +0100

    Initial Commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Muhamed Zahiri on 21.02.22.
+//
+
+import Foundation
+
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
